8064444d41f659110c01010e4839455b014d0220
Flo Salinas <florenciabsalinas@gmail.com>
2016-12-11 18:23:21 -0500

    Add probability manager. Requires adding logic so first pop up can only be fact.

--- Assets/Scripts/ProbabilityManager.cs
+++ Assets/Scripts/ProbabilityManager.cs
@@ -6,6 +6,10 @@ public class ProbabilityManager : MonoBehaviour {
 	public int maxFactPerLevel;
 	//note that question = 0 and fact =1
 	public enum PopUpType { QUESTION, FACT};
+	//prob * 10 = %
+	public int QUESTIONPROB = 6;
+	public int FACTPROB = 4;
+	public bool firstCoin;
 
 
 
@@ -13,6 +17,7 @@ public class ProbabilityManager : MonoBehaviour {
 	void Start () {
 		maxQuestPerLevel = 10;
 		maxFactPerLevel = 10;
+		firstCoin = true;
 	}
 	
 	// Update is called once per frame
@@ -26,10 +31,9 @@ public class ProbabilityManager : MonoBehaviour {
 	*/
 	public PopUpType getPopUpType() {
 		
-		int popUpType = Random.Range (0, 5);
+		int popUpType = Random.Range (1, 10);
 
-		if (popUpType < 2) {
-			
+		if (popUpType > 4 && popUpType < 11) {
 			PopUp pop = GetComponentInParent<PopUp> ();
 			pop.activateCanvas ();
 			return PopUpType.QUESTION;
