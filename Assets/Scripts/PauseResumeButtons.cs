a7a9569ddcb930f648e80dc2aa4a7b2d4840bad1
Flo Salinas <florenciabsalinas@gmail.com>
2016-12-11 23:19:38 -0500

    Add pause and resume buttons. Add menu in the future.

---
+++ Assets/Scripts/PauseResumeButtons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseResumeButtons : MonoBehaviour {
+	bool isPaused;
+
+	// Use this for initialization
+	void Start () {
+		isPaused = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void PauseGame(){
+		if(!isPaused){
+			isPaused = true;
+			Time.timeScale = 0.0f;
+		}
+	}
+
+	public void ResumeGame(){
+		if(isPaused){
+			isPaused = false;
+			Time.timeScale = 1.0f;
+		}
+	}
+
+}
